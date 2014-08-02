68fb0315d816bead9b8f2cda575cb4e2e7824680
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-08-03 00:11:56 +0300

    some

--- db.hs
+++ db.hs
@@ -5,26 +5,26 @@
 module DB where
 
 import Model
-import Control.Monad.IO.Class  (liftIO)
+import Data.Text
 import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite as SQ
+import Control.Monad.IO.Class  (liftIO)
 
-import qualified Data.Text as T
 
 initDB = SQ.runSqlite "mydb.sqlite" $ do
 	SQ.runMigration migrateAll
 
 runDB action = SQ.runSqlite "mydb.sqlite" $ action
 
-insertPerson p = SQ.insert $ p
+insertPerson p = runDB $ SQ.insert $ p
 
-newPID :: T.Text -> Int -> Int -> Int -> Leaderboard
-newPID id s1 s2 s3 = Leaderboard id s1 s2 s3
+newPerson :: Text -> Int -> Int -> Int -> Leaderboard
+newPerson id s1 s2 s3 = Leaderboard id s1 s2 s3
 
-getByID (pid :: T.Text) = selectList [LeaderboardName ==. pid] []--[LimitTo 1]
+getByID (pid :: Text) = runDB $ selectList [LeaderboardName ==. pid] []--[LimitTo 1]
 
-updateScore' (pid :: T.Text) b (score :: Int) = updateWhere [LeaderboardName ==. pid] [b =. score]
+updateScore' (pid :: Text) b (score :: Int) = updateWhere [LeaderboardName ==. pid] [b =. score]
 
 extract [] = []
 extract (e:ent) = eV : extract ent
--- main.hs
+++ main.hs
@@ -4,13 +4,12 @@
 
 module Main where
 
-import Web.Spock
 import Model
-import qualified DB
-import Control.Monad.IO.Class
-import qualified Data.Text as T
+import Web.Spock
+import Data.Text
+import Control.Monad.IO.Class (liftIO)
 
-import qualified Database.Persist.Sqlite as SQ
+import qualified DB
 
 dPort = 3000
 
@@ -32,12 +31,12 @@ runUrl = do
 mainPage = html $ "<center><h1> Hi There! Well, you should go. </h1></center>"
 
 getPlayer = do
-	(Just pID :: Maybe T.Text) <- param "pID"
-	a <- liftIO $ DB.runDB $ DB.getByID pID
-	json $ DB.extract a
+	(Just pID :: Maybe Text) <- param "pID"
+	out <- liftIO $ DB.getByID pID
+	json $ DB.extract out
 
 postScore = do
-	(Just id :: Maybe T.Text) <- param "pID"
+	(Just id :: Maybe Text) <- param "pID"
 	(Just s1 :: Maybe Int)    <- param "sE"
 	(Just s2 :: Maybe Int)    <- param "sM"
 	(Just s3 :: Maybe Int)    <- param "sH"
@@ -45,16 +44,16 @@ postScore = do
 	jSucces
 
 updateScore rec = do 
-			(Just id :: Maybe T.Text) <- param "pID"
+			(Just id :: Maybe Text) <- param "pID"
 			(Just score :: Maybe Int) <- param "nScore"
 			liftIO $ DB.runDB $ DB.updateScore' id rec score
 			jSucces
 
-addPlayer player = liftIO $ DB.runDB $ DB.insertPerson player
+addPlayer player = liftIO $ DB.insertPerson player
 
 emptyPage = text ""
 
-jSucces = json $ ["done" :: T.Text]
+jSucces = json $ ["done" :: Text]
 
-newPID :: T.Text -> Int -> Int -> Int -> Leaderboard
+newPID :: Text -> Int -> Int -> Int -> Leaderboard
 newPID id s1 s2 s3 = Leaderboard id s1 s2 s3
