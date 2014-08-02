3ba25abcf6587ada608aa6f817a3a61fbe316eba
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-08-02 17:14:21 +0300

    to json!

--- db.hs
+++ db.hs
@@ -1,16 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 module DB where
 
 import Model
-import qualified Database.Persist.Sqlite as SQ
+import Control.Monad.IO.Class  (liftIO)
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Sqlite as SQ
+
+import qualified Data.Text as T
 
 initDB = SQ.runSqlite "mydb.sqlite" $ do
 	SQ.runMigration migrateAll
 
 runDB action = SQ.runSqlite "mydb.sqlite" $ action
 
-insertPerson p s1 s2 s3 = SQ.insert $ Leaderboard p s1 s2 s3
+insertPerson p = SQ.insert $ p
 
-person = Leaderboard "Stan" 5 15 45
+newPID :: T.Text -> Int -> Int -> Int -> Leaderboard
+newPID id s1 s2 s3 = Leaderboard id s1 s2 s3
+
+getByID (pid :: T.Text) = selectList [LeaderboardName ==. pid] []--[LimitTo 1]
+
+--extract :: [Entity] -> [Leaderboard]
+extract [] = []
+extract (e:ent) = eV : extract ent
+	where
+		eV = entityVal e
+
+{-
+extract (e:ent) = (newPID (leaderboardName eV) (leaderboardScoreEasy eV) (leaderboardScoreMedium eV) (leaderboardScoreHard eV) ) : extract ent
+	where
+		eV = entityVal e
+-}
--- main.hs
+++ main.hs
@@ -5,9 +5,12 @@
 module Main where
 
 import Web.Spock
+import Model
 import qualified DB
 import Control.Monad.IO.Class
 import qualified Data.Text as T
+
+import qualified Database.Persist.Sqlite as SQ
 
 dPort = 3000
 
@@ -16,23 +19,35 @@ main = do
 	putStrLn "To exit press [CTRL + C]"
 	spockT dPort id $ runUrl
 
-runUrl = do 
+runUrl = do
 		get "/" mainPage
-		get "/get" emptyPage
-		post "/postScore" $ do 
-									(Just id :: Maybe T.Text) <- param "id"
-									(Just s1 :: Maybe Int) <- param "scoreE"
-									(Just s2 :: Maybe Int) <- param "scoreM"
-									(Just s3 :: Maybe Int) <- param "scoreH"
-									postScore id s1 s2 s3
+		get "/get/:pID" getPlayer
+		post "/postScore" postScore
 
 --------------------------------------------
 
-mainPage = html $ "<center><h1> Hi There! </h1></center>"
+mainPage = html $ "<center><h1> Hi There! Well, you should go. </h1></center>"
 
-postScore :: MonadIO m => T.Text -> Int -> Int -> Int -> ActionT m ()
-postScore id s1 s2 s3 = do
-			liftIO $ DB.runDB $ DB.insertPerson id s1 s2 s3
-			text ""
+getPlayer = do
+	(Just pID :: Maybe T.Text) <- param "pID"
+	a <- liftIO $ DB.runDB $ DB.getByID pID
+	b <- liftIO $ print $ DB.extract a
+	--b <- liftIO $ return DB.extract a
+	json $ [ "asd" :: T.Text]
+
+postScore = do
+	(Just id :: Maybe T.Text) <- param "pID"
+	(Just s1 :: Maybe Int)    <- param "sE"
+	(Just s2 :: Maybe Int)    <- param "sM"
+	(Just s3 :: Maybe Int)    <- param "sH"
+	addPlayer $ newPID "id" s1 s2 s3	
+	jSucces
+
+addPlayer player = liftIO $ DB.runDB $ DB.insertPerson player
 
 emptyPage = text ""
+
+jSucces = json $ ["done" :: T.Text]
+
+newPID :: T.Text -> Int -> Int -> Int -> Leaderboard
+newPID id s1 s2 s3 = Leaderboard id s1 s2 s3
